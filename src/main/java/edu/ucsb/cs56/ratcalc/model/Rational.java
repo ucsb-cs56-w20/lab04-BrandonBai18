cf6011523d79ec7d54a861f25a89bc837f8c9176
Shi Bai <shibai@umail.ucsb.edu>
2020-02-02 14:33:35 -0800

    a

--- src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
+++ src/main/java/edu/ucsb/cs56/ratcalc/model/Rational.java
@@ -1,12 +1,5 @@
 package edu.ucsb.cs56.ratcalc.model;
 
-/**
- * A class to represent a rational number with a numerator and denominator
- *
- * @author P. Conrad for CS56 F16
- *
- */
-
 public class Rational {
 
     private int num;
@@ -44,6 +37,10 @@ public class Rational {
             this.num /= gcd;
             this.denom /= gcd;
         }
+	if(this.num>=0 && this.denom<0){
+	    this.num = this.num*(-1);
+	    this.denom = this.denom*(-1);
+	}
     }
 
     public String toString() {
@@ -65,10 +62,32 @@ public class Rational {
     }
 
     public static Rational product(Rational a, Rational b) {
-	return new Rational(a.num * b.num, a.denom * b.denom);
+        return new Rational(a.num * b.num, a.denom * b.denom);
     }
 
-    public static Rational sum(Rational a, Rational b) {
+    /**
+     * For testing getters.
+     *
+     * 
+     */
+    public static int lcm(int a,int b){
+	return (a*b)/gcd(a,b);
+    }
+
+    public Rational plus(Rational r){
+	int n1;
+	int n2;
+	n1 = this.denom * r.getNumerator() + this.num * r.getDenominator();
+	n2 = this.denom * r.getDenominator();
+	//if (n1 >= 0 && n2 < 0){
+	//    n1 = n1 * -1;
+	//    n2 = n2 * -1;
+	//}
+	Rational newone = new Rational(n1,n2);
+	return newone;
+    }
+
+    public static Rational sum(Rational a, Rational b){
 	int n1;
 	int n2;
 	n1 = a.getDenominator() * b.getNumerator() + a.getNumerator() * b.getDenominator();
@@ -82,47 +101,14 @@ public class Rational {
 	return newone;
     }
 
-    public static Rational difference(Rational a, Rational b) {
-	Rational tR = new Rational(b.getNumerator()*(-1),b.getDenominator());
-	return sum(a, tR);
-    }
-
-    public static Rational quotient(Rational a, Rational b) {
-	Rational newone = new Rational(a.num*b.denom,a.denom*b.num);
-        return newone;
-    }
-    /**
-     * For testing getters.
-     *
-     * @param args unused
-     */
-
-    public static void main(String[] args) {
-        Rational r = new Rational(5, 7);
-        System.out.println("r.getNumerator()=" + r.getNumerator());
-        System.out.println("r.getDenominator()=" + r.getDenominator());
-    }
-
-    public static int lcm(int a,int b){
-	return (a*b)/gcd(a,b);
-    }
-
-    public Rational plus(Rational r){
-	int n1;
-	int n2;
-	n1 = this.denom * r.getNumerator() + this.num * r.getDenominator();
-	n2 = this.denom * r.getDenominator();
-	if (n1 >= 0 && n2 < 0){
-	    n1 = n1 * -1;
-	    n2 = n2 * -1;
-	}
-	Rational newone = new Rational(n1,n2);
-	return newone;
-    }
-
     public Rational minus(Rational r){
 	Rational tR = new Rational(r.getNumerator()*(-1),r.getDenominator());
 	return plus(tR);
+    }
+    
+    public static Rational difference(Rational a, Rational b){
+	Rational tR = new Rational(b.getNumerator()*(-1),b.getDenominator());
+	return sum(a, tR);
     }
 
     public Rational reciprocalOf(){
@@ -137,8 +123,19 @@ public class Rational {
 	Rational newone = new Rational(this.num*r.denom,this.denom*r.num);
         return newone;
     }
+
+    public static Rational quotient(Rational a, Rational b){
+	Rational newone = new Rational(a.num*b.denom,a.denom*b.num);
+        return newone;
+    }
     
+    
+
+    public static void main(String[] args) {
+        Rational r = new Rational(5, 7);
+        System.out.println("r.getNumerator()=" + r.getNumerator());
+        System.out.println("r.getDenominator()=" + r.getDenominator());
+
+    }
+
 }
-
-
-
